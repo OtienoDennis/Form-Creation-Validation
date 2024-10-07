a531d61fe616314140d3f24e8a6c931bbddd8105
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-10-07 21:39:15 +0300

    Update success message

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ document.addEventListener( "DOMContentLoaded", function () {
         }
         feedbackDiv.style.display = 'block';
         if ( isValid ) {
-            feedbackDiv.textContent = "Registration succesful!";
+            feedbackDiv.textContent = "Registration successful!";
             feedbackDiv.style.color = '#28a745';
         } else {
             feedbackDiv.innerHTML = `
